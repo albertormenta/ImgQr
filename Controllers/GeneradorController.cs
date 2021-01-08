8473c37970d4df9f508e882a81414396cb39a2c1
Alberto Martínez <ajmartinez@cre.gob.mx>
2021-01-07 20:32:41 -0600

    Add project files.

---
+++ Controllers/GeneradorController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using QRGenerator.Helper;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace QRGenerator.Controllers
+{
+    [Route("api/Controller")]
+    [ApiController]
+    public class GeneradorController : Controller
+    {
+        [HttpGet("QRCode")]
+        public IActionResult Image()
+        {
+            GeneradorHelper generador = new GeneradorHelper();
+            Byte[] qr = generador.GererarQR();
+
+            return File(qr,"image/png");
+
+        }
+    }
+}
---
+++ Helper/GeneradorHelper.cs
@@ -0,0 +1,45 @@
+﻿using QRCoder;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using static QRCoder.PayloadGenerator;
+
+namespace QRGenerator.Helper
+{
+    public class GeneradorHelper
+    {
+        public GeneradorHelper()
+        {
+
+        }
+        public Byte[] GererarQR()
+        {
+            WiFi wifi = new WiFi("nombredelared", "lapass", WiFi.Authentication.WPA);
+
+            QRCodeGenerator qrGenerator = new QRCodeGenerator();
+            QRCodeData qrCodeData = qrGenerator.CreateQrCode(wifi, QRCodeGenerator.ECCLevel.Q);
+            QRCode qrCode = new QRCode(qrCodeData);
+
+            Bitmap qrCodeAsByteArray;
+
+            using (var qrBitmap = new BitmapByteQRCode(qrCodeData))
+            {
+                qrCodeAsByteArray = qrCode.GetGraphic(20, "#FF0000", "#FFFFFF");
+
+                MemoryStream stream = new MemoryStream();
+                qrCodeAsByteArray.Save(stream, ImageFormat.Png);
+                byte[] result = stream.ToArray();
+                return result;
+            }
+
+
+        }
+
+    }
+
+
+}
